785f097ce4a2acfea5282a2d0669efff683c60b3
aabhinaav <aabhinaavgarg@gmail.com>
2023-08-07 07:53:53 +0530

    api calling added

---
+++ lib/Utils/api_provider.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:b_api_calling/Exceptions/custom_exception.dart';
+import 'package:b_api_calling/Models/posts.dart';
+import 'package:http/http.dart' as httpObj;
+
+class ApiProvider {
+  static Future<List<Post>> getPosts() async {
+    dynamic posts;
+
+    try {
+      String apiUrl = "https://jsonplaceholder.typicode.com/posts";
+      final response =
+          await httpObj.get(Uri.parse(Uri.encodeFull(apiUrl)), headers: {
+        'content-type': 'application/json',
+      });
+
+      posts = extractResponse(response);
+    } on SocketException catch (e) {
+      print("Error in calling API : " + e.message);
+    }
+
+    return posts;
+  }
+
+  static dynamic extractResponse(httpObj.Response response) {
+    List<Post> posts = [];
+
+    switch (response.statusCode) {
+      case 200:
+        {
+          final dynamic rawData = json.decode(response.body.toString());
+
+          posts.clear();
+          for (Map<String, dynamic> singleData in rawData) {
+            posts.add(Post.fromMap(singleData));
+          }
+
+          return posts as dynamic;
+        }
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 401:
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      default:
+        throw FetchDataException(
+            "Error in communication with server:-\nStatus Code : " +
+                response.statusCode.toString());
+    }
+  }
+}
